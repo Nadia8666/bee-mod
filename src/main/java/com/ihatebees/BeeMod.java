ba9373de7e5ea54334fad694bc651fa234d27dd4
ACKREIK <jerifyemail@gmail.com>
2024-06-13 17:48:41 -0700

    h

--- src/main/java/com/ihatebees/BeeMod.java
+++ src/main/java/com/ihatebees/BeeMod.java
@@ -10,6 +10,7 @@ public class BeeMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("beemod");
+	public static final String MODID = "MOD_ID";
 
 	@Override
 	public void onInitialize() {
---
+++ src/main/java/com/ihatebees/item/ModItems.java
@@ -0,0 +1,29 @@
+package com.ihatebees.item;
+
+import com.ihatebees.BeeMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item TestThing = registerItem("testthing", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(TestThing);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(BeeMod.MODID, name), item);
+    }
+
+    public static void registerModItems() {
+        BeeMod.LOGGER.info("Registering Mod Items for " + BeeMod.MODID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
