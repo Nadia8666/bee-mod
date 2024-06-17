8000b6fe9d6fc4b14b094626a21be1c477437adc
ACKREIK <jerifyemail@gmail.com>
2024-06-16 19:00:33 -0700

    fix star sweep particle, rename and add cancel mixin

--- src/main/java/com/ihatebees/mixin/HitEffectMixin.java
+++ src/main/java/com/ihatebees/mixin/HitEffectMixin.java
@@ -1,10 +1,8 @@
 package com.ihatebees.mixin;
 
 import com.ihatebees.item.custom.DarkScytheSwordItem;
-import com.ihatebees.item.custom.StarSawSwordItem;
 import com.ihatebees.particle.ModParticles;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -16,23 +14,27 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
-public abstract class helloyesmixin extends LivingEntity {
-	protected helloyesmixin(EntityType<? extends LivingEntity> entityType, World world) {
+public abstract class HitEffectMixin extends LivingEntity {
+	protected HitEffectMixin(EntityType<? extends LivingEntity> entityType, World world) {
 		super(entityType, world);
 	}
 
-	@Inject(at = @At("HEAD"), method = "spawnSweepAttackParticles")
+	@Inject(at = @At("HEAD"), method = "spawnSweepAttackParticles", cancellable = true)
 	private void onSweep(CallbackInfo cir) {
+		boolean did_trigger = false;
 		if (getMainHandStack().getItem() instanceof DarkScytheSwordItem item) {
 			double d = (double) (-MathHelper.sin(this.getYaw() * 0.017453292F));
 			double e = (double) MathHelper.cos(this.getYaw() * 0.017453292F);
 			if (this.getWorld() instanceof ServerWorld) {
-				((ServerWorld) this.getWorld()).spawnParticles(ModParticles.STARSWEEP, this.getX() + d, this.getBodyY(0.5), this.getZ() + e, 0, d, 0.0, e, 0.0);
+				((ServerWorld) this.getWorld()).spawnParticles(ModParticles.STARSWEEP_PARTICLE, this.getX() + d, this.getBodyY(0.5), this.getZ() + e, 0, d, 0.0, e, 0.0);
 			}
-		}
-	}
-	@Inject(at = @At("HEAD"), method = "attack")
-	private void alala(CallbackInfo cir) {
 
+			did_trigger = true;
+		}
+
+		// if any of the items activated their custom effects, stop the default effect from happening
+		if (did_trigger) {
+			cir.cancel();
+		}
 	}
 }
---
+++ src/main/java/com/ihatebees/particle/custom/StarSweepParticle.java
@@ -0,0 +1,55 @@
+package com.ihatebees.particle.custom;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.particle.*;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.particle.DefaultParticleType;
+
+public class StarSweepParticle extends SpriteBillboardParticle {
+    private final SpriteProvider spriteProvider;
+
+    StarSweepParticle(ClientWorld world, double x, double y, double z, double d, SpriteProvider spriteProvider) {
+        super(world, x, y, z, 0.0, 0.0, 0.0);
+        this.spriteProvider = spriteProvider;
+        this.maxAge = 4;
+        float f = this.random.nextFloat() * 0.6F + 0.4F;
+        this.red = f;
+        this.green = f;
+        this.blue = f;
+        this.scale = 1.0F - (float)d * 0.5F;
+        this.setSpriteForAge(spriteProvider);
+    }
+
+    public int getBrightness(float tint) {
+        return 15728880;
+    }
+
+    public void tick() {
+        this.prevPosX = this.x;
+        this.prevPosY = this.y;
+        this.prevPosZ = this.z;
+        if (this.age++ >= this.maxAge) {
+            this.markDead();
+        } else {
+            this.setSpriteForAge(this.spriteProvider);
+        }
+    }
+
+    public ParticleTextureSheet getType() {
+        return ParticleTextureSheet.PARTICLE_SHEET_LIT;
+    }
+
+    @Environment(EnvType.CLIENT)
+    public static class Factory implements ParticleFactory<DefaultParticleType> {
+        private final SpriteProvider spriteProvider;
+
+        public Factory(SpriteProvider spriteProvider) {
+            this.spriteProvider = spriteProvider;
+        }
+
+        public Particle createParticle(DefaultParticleType defaultParticleType, ClientWorld clientWorld, double d, double e, double f, double g, double h, double i) {
+            return new StarSweepParticle(clientWorld, d, e, f, g, this.spriteProvider);
+        }
+    }
+}
